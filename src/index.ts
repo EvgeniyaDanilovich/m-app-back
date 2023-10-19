d7e70f48aaf1f7af02d0b3612b1b79022cbad093
Evgeniya Danilovich <68774339+EvgeniyaDanilovich@users.noreply.github.com>
2023-10-19 15:12:08 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import * as serverService from './services/server.service';
 (async () => {
   try {
     // await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
-    await mongoose.connect('mongodb+srv://edanilovich:mCW_6qxCk7WBjCw@cluster0.toypvc3.mongodb.net/managerApp');
+    // await mongoose.connect('mongodb+srv://edanilovich:mCW_6qxCk7WBjCw@cluster0.toypvc3.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://evgeniyashpadaruk:6A27Gu9pUj77mX5r@m-app-back.v8brtcd.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
